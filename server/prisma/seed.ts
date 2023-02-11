e6cb0c70de7744848b1cfb49f60f791856fb4a1d
Gabriel Dias <gabrielmoreiradias270402@gmail.com>
2023-02-11 14:51:34 -0300

    Update web icon

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -3,13 +3,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const firstHabitId = '0730ffac-d039-4194-9571-01aa2aa0efbd'
-const firstHabitCreationDate = new Date('2022-12-31T03:00:00.000')
-
-const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297'
-const secondHabitCreationDate = new Date('2023-01-03T03:00:00.000')
-
-const thirdHabitId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00'
-const thirdHabitCreationDate = new Date('2023-01-08T03:00:00.000')
+const firstHabitCreationDate = new Date('2023-02-10T03:00:00.000')
 
 
 async function run() {
@@ -25,79 +19,28 @@ async function run() {
         created_: firstHabitCreationDate,
         weekDays: {
           create: [
-            { week_day: 1 },
-            { week_day: 2 },
-            { week_day: 3 },
-          ]
-        }
-      }
-    }),
-
-    prisma.habit.create({
-      data: {
-        id: secondHabitId,
-        title: 'Exercitar',
-        created_: secondHabitCreationDate,
-        weekDays: {
-          create: [
-            { week_day: 3 },
-            { week_day: 4 },
-            { week_day: 5 },
-          ]
-        }
-      }
-    }),
-
-    prisma.habit.create({
-      data: {
-        id: thirdHabitId,
-        title: 'Dormir 8h',
-        created_: thirdHabitCreationDate,
-        weekDays: {
-          create: [
+            { week_day: 0 },
             { week_day: 1 },
             { week_day: 2 },
             { week_day: 3 },
             { week_day: 4 },
             { week_day: 5 },
+            { week_day: 6 },
           ]
         }
       }
-    })
+    }),
   ])
 
   await Promise.all([
     prisma.day.create({
       data: {
         
-        date: new Date('2023-01-02T03:00:00.000z'),
+        date: new Date('2023-02-11T03:00:00.000z'),
         DayHabits: {
           create: {
             habit_id: firstHabitId,
           }
-        }
-      }
-    }),
-
-    prisma.day.create({
-      data: {
-        date: new Date('2023-01-06T03:00:00.000z'),
-        DayHabits: {
-          create: {
-            habit_id: firstHabitId,
-          }
-        }
-      }
-    }),
-
-    prisma.day.create({
-      data: {
-        date: new Date('2023-01-04T03:00:00.000z'),
-        DayHabits: {
-          create: [
-            { habit_id: firstHabitId },
-            { habit_id: secondHabitId },
-          ]
         }
       }
     }),
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import { prisma } from './lib/prisma'
 import { appRoutes } from "./routes";
 
 const app = Fastify();
+const port = (process.env.PORT || 3333)
 
 app.register(cors);
 app.register(appRoutes)
@@ -12,8 +13,8 @@ app.register(appRoutes)
 
 //Configura Porta e mensagem no console
 app.listen({
-    port:3333,
+    port:port,
     host: '0.0.0.0',
 }).then(()=>{
-    console.log("HTTP Server running")
+    console.log("HTTP Server running on Port: " + port)
 })
